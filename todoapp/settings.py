57a669b3c15d20978869eb456cc0b0280aa84a37
sreekaanth <sreekaanthgnsn3@gmail.com>
2021-04-24 22:52:14 -0400

    Updated settings.py

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = ''
 STATIC_URL = '/static/'
 #STATIC_URL = '/static/'
 
